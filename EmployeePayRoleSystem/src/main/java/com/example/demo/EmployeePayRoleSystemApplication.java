70a31b0ddff0e25a2468ebc6dbc448b6241a39bb
Gobinda bhole <MY_NAME@example.com>
2024-12-05 18:41:24 +0530

    commit by hp freamonly

---
+++ EmployeePayRoleSystem/src/main/java/com/example/demo/EmployeePayRoleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayRoleSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayRoleSystemApplication.class, args);
+	}
+
+}
